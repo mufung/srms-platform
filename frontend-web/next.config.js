64bc66e9633ccf3178384dcf97cc0a9a43839f9d
mufung <mufungangelbell@gmail.com>
2026-04-17 06:19:14 +0100

    Fix Amplify - static export with out directory

--- frontend-web/next.config.js
+++ frontend-web/next.config.js
@@ -1,32 +1,9 @@
  /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-
+  output: 'export',
+  trailingSlash: true,
   images: {
-    domains: [
-      'srms-master-assets.s3.amazonaws.com',
-      'srms-master-assets.s3.us-east-1.amazonaws.com',
-    ],
-    formats: ['image/avif', 'image/webp'],
-  },
-
-  env: {
-    NEXT_PUBLIC_PLATFORM_NAME: process.env.NEXT_PUBLIC_PLATFORM_NAME || 'SRMS Platform',
-    NEXT_PUBLIC_OWNER_NAME: 'MUFUNG ANGELBELL MBUYEH',
-    NEXT_PUBLIC_OWNER_EMAIL: 'mufungangelbellmbuyeh@gmail.com',
-    NEXT_PUBLIC_OWNER_WHATSAPP: '+237671534067',
-    NEXT_PUBLIC_OWNER_LATITUDE: '3.8480',
-    NEXT_PUBLIC_OWNER_LONGITUDE: '11.5021',
-    NEXT_PUBLIC_OWNER_LOCATION: 'Yaoundé, Cameroon Northwest',
-    NEXT_PUBLIC_OWNER_TITLE: 'AWS Solutions Architect',
-  },
-
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      canvas: false,
-    };
-    return config;
+    unoptimized: true,
   },
 };
 
